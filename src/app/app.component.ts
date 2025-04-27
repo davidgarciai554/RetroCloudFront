8e270bd8280b578ac4926b7807d061076a4bbd97
davidgarciai554 <garciamartindavid@hotmail.es>
2025-04-27 18:35:23 +0200

    feat: add Resumen component and integrate Angular Material
    - Added @angular/cdk and @angular/material dependencies to package.json.
    - Created ResumenComponent with associated HTML, CSS, and TypeScript files.
    - Updated app.component.html to replace placeholder content with <app-resumen> component.
    - Removed app.component.spec.ts as it is no longer needed.
    - Added favicon image to assets.
    - Updated index.html to include Google Fonts and Material Icons.
    - Modified global styles in styles.css for better layout and font styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResumenComponent } from "./resumen/resumen.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ ResumenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
